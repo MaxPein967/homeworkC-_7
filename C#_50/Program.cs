d64a11ecb45a2299e0b29d5f319d58fd18d2ef89
Sergei Vasiliev <9670105747@mail.ru>
2023-02-20 11:36:26 +0700

    Task 50

--- C#_50/Program.cs
+++ C#_50/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+Console.Clear();
+Console.Write("Введите строку: ");
+int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Введите столбец: ");
+int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
+int n = 5;
+int m = 7;
+Random random = new Random();
+int[,] arr = new int[n, m];
+Console.WriteLine("Исходный массив: ");
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = random.Next(10, 99);
+        Console.Write("{0} ", arr[i, j]);
+    }
+    Console.WriteLine();
+}
+Console.Write("Введите элемент массива: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x == arr[pos1, pos2]) Console.Write(arr[pos1, pos2]);
+else Console.WriteLine("Элемент не существует  ");
+
