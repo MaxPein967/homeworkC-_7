34e208b43b002fdb847a46ad13b0e5d1fa5001e2
Sergei Vasiliev <9670105747@mail.ru>
2023-02-20 11:36:59 +0700

    Task 52

--- C#_52/Program.cs
+++ C#_52/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов 
+// в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+int n = ReadInt("Введите количество строк: ");
+int m = ReadInt("Введите количество столбцов: ");
+
+int[,] nums = new int[n, m];
+Console.WriteLine();
+FillArrayRandomNumbers(nums);
+PrintArray(nums);
+Console.WriteLine();
+
+for (int j = 0; j < nums.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < nums.GetLength(0); i++)
+    {
+        sum += nums[i, j];
+    }
+    Console.Write($" { Math.Round(sum / nums.GetLength(0), 1)} ");
+}
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToInt32(new Random().Next(10, 100));
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine(" ");
+    }
+}
+
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
